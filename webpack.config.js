2e5d9f9f39fb12d42791bea53e8e2ff263cec77d
Luca Bacchi <bacchilu@gmail.com>
2020-10-31 15:28:53 +0000

    Recompiled all

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,54 @@
 const path = require('path');
 
-module.exports = {
-    entry: {
-        app: './src/index.js'
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'public')
-    },
-    devtool: 'inline-source-map',
-    mode: "development",
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ["@babel/preset-env"]
-            }
-        }]
+module.exports = function (env) {
+    const res = {
+        entry: {
+            app: './src/index.js'
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'public')
+        },
+
+        // // I need this to use "exceljs"
+        // node: {fs: "empty"},
+
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ["@babel/preset-env", {
+                                debug: true,
+                                useBuiltIns: 'usage',
+                                corejs: 3,
+                                targets: {
+                                    browsers: ["> 5%", "last 2 versions"]
+                                }
+                            }],
+                            ["@babel/preset-react"]
+                        ],
+                        plugins: ['transform-commonjs-es2015-modules']
+                    }
+                }
+            }]
+        }
+    };
+    if (env.development) {
+        res['mode'] = 'development';
+        res['devtool'] = 'inline-source-map';
     }
+    if (env.production) {
+        res['mode'] = 'production';
+        // res['plugins'] = [
+        //     new (require('webpack')).DefinePlugin({
+        //         'process.env.NODE_ENV': JSON.stringify('production')
+        //     })
+        // ];
+    }
+
+    return res;
 };
