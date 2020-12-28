4a04c8eccb7c775d9debf5a30624725dc86479a4
Luca Bacchi <bacchilu@gmail.com>
2020-12-28 16:03:25 +0000

    Test authentication

--- src/model.js
+++ src/model.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 
-// require('firebase/auth');
+import 'firebase/auth';
+
 import 'firebase/firestore';
 
 const firebaseConfig = {
@@ -14,6 +15,28 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+
+// const provider = new firebase.auth.GoogleAuthProvider();
+// firebase
+//     .auth()
+//     .signInWithPopup(provider)
+//     .then(function (result) {
+//         // This gives you a Google Access Token. You can use it to access the Google API.
+//         const token = result.credential.accessToken;
+//         // The signed-in user info.
+//         const user = result.user;
+//         console.log(user);
+//     })
+//     .catch(function (error) {
+//         // Handle Errors here.
+//         const errorCode = error.code;
+//         const errorMessage = error.message;
+//         // The email of the user's account used.
+//         const email = error.email;
+//         // The firebase.auth.AuthCredential type that was used.
+//         const credential = error.credential;
+//         // ...
+//     });
 
 const db = firebase.firestore();
 
