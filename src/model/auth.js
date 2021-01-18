0b286539221a89ef9c673044a96282df58bdc448
Luca Bacchi <bacchilu@gmail.com>
2021-01-18 15:36:23 +0000

    Refactoring

--- src/model/auth.js
+++ src/model/auth.js
@@ -2,22 +2,18 @@ import 'firebase/auth';
 
 export const Auth = function (firebase) {
     return {
-        signIn: function () {
+        signIn: async function () {
             const provider = new firebase.auth.GoogleAuthProvider();
             provider.setCustomParameters({prompt: 'select_account'});
-            firebase
-                .auth()
-                .signInWithPopup(provider)
-                // .signInWithRedirect(provider)
-                .then(function (result) {
-                    const user = result.user;
-                    console.log(user['displayName'], user['email'], user['photoURL']);
-                })
-                .catch(function (error) {
-                    const errorCode = error.code;
-                    const errorMessage = error.message;
-                    console.log(errorCode, errorMessage);
-                });
+            try {
+                const result = await firebase.auth().signInWithRedirect(provider);
+                const user = result.user;
+                console.log(user['displayName'], user['email'], user['photoURL']);
+            } catch (error) {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+            }
         },
         signOut: function () {
             firebase.auth().signOut();
