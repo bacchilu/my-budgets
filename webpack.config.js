fe98c15ce351701fd34e73ac4c489f9008decbb5
Luca Bacchi <bacchilu@gmail.com>
2021-03-04 15:03:58 +0000

    Refactoring to do public on github pages

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = function (env) {
         },
         output: {
             filename: '[name].js',
-            path: path.resolve(__dirname, 'public'),
+            path: path.resolve(__dirname, 'dist'),
         },
         module: {
             rules: [
