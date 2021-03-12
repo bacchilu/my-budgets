9f14a1b66b6bbbca52789daf85298ccdd5208b5a
Luca Bacchi <bacchilu@gmail.com>
2021-03-12 15:48:39 +0000

    Testing how to wrap a bootstrap modal in react

--- src/main/navbar.js
+++ src/main/navbar.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 import {toCurrency, Login} from '../utils.js';
 import {confirm} from '../modal.js';
+import {Modal} from '../libs/modal.js';
 
 export const NavBar = function ({user, budgets, Methods}) {
+    const [modalOpened, setModalOpened] = React.useState(false);
+
     const rechargeAll = function () {
         confirm(`Sicuro di voler ricaricare tutti i budget?`, function () {
             budgets.forEach(function (budget, index) {
@@ -12,12 +15,36 @@ export const NavBar = function ({user, budgets, Methods}) {
         });
     };
 
+    const showModalTest = function () {
+        setModalOpened(true);
+    };
+    const hideModalTest = function () {
+        setModalOpened(false);
+    };
+
     const totalWeeklyBudget = budgets.reduce(function (acc, item) {
         return acc + item['weekly_budget'];
     }, 0);
 
     return (
         <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+            <Modal opened={modalOpened} setOpened={setModalOpened}>
+                <div className="modal-header">
+                    <h5 className="modal-title">TEST MODAL</h5>
+                    <button className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                    <p>TODO</p>
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+                        Close
+                    </button>
+                    <button type="button" className="btn btn-primary" onClick={hideModalTest}>
+                        Save changes
+                    </button>
+                </div>
+            </Modal>
             <div className="container-fluid">
                 <span className="navbar-brand">
                     My Budgets <sub>{toCurrency(totalWeeklyBudget)}</sub>
@@ -50,6 +77,18 @@ export const NavBar = function ({user, budgets, Methods}) {
                         <li className="nav-item">
                             <a className="nav-link disabled" href="#" aria-disabled="true">
                                 New budget
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                className="nav-link"
+                                href="#"
+                                onClick={function (e) {
+                                    e.preventDefault();
+                                    showModalTest();
+                                }}
+                            >
+                                Modal Test
                             </a>
                         </li>
                     </ul>
