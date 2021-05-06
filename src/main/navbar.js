03362efde59b7fbd420721e9362cce6aa949c073
Luca Bacchi <bacchilu@gmail.com>
2021-05-06 15:19:08 +0000

    Draft modal for new budget

--- src/main/navbar.js
+++ src/main/navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {toCurrency, Login} from '../utils.js';
 import {Modal} from '../libs/modal.js';
 
-const Confirm = function ({opened, setOpened, action}) {
+const RechargeAllModal = function ({opened, setOpened, action}) {
     const onClick = function () {
         action();
         setOpened(false);
@@ -12,7 +12,7 @@ const Confirm = function ({opened, setOpened, action}) {
     return (
         <Modal opened={opened} setOpened={setOpened}>
             <div className="modal-header">
-                <h5 className="modal-title">Confirm</h5>
+                <h5 className="modal-title">Recharge All</h5>
                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">Sicuro di voler ricaricare tutti i budget?</div>
@@ -25,8 +25,31 @@ const Confirm = function ({opened, setOpened, action}) {
     );
 };
 
+const NewBudgetModal = function ({opened, setOpened, action}) {
+    const onClick = function () {
+        action();
+        setOpened(false);
+    };
+
+    return (
+        <Modal opened={opened} setOpened={setOpened}>
+            <div className="modal-header">
+                <h5 className="modal-title">New Budget</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">TODO</div>
+            <div className="modal-footer" style={{display: 'block'}}>
+                <button className="btn btn-outline-danger float-end" onClick={onClick}>
+                    Ok
+                </button>
+            </div>
+        </Modal>
+    );
+};
+
 export const NavBar = function ({user, budgets, Methods}) {
-    const [modalOpened, setModalOpened] = React.useState(false);
+    const [rechargeAllModalOpened, setRechargeAllModalOpened] = React.useState(false);
+    const [newBudgetModalOpened, setNewBudgetModalOpened] = React.useState(false);
 
     const rechargeAll = function () {
         budgets.forEach(function (budget) {
@@ -34,8 +57,14 @@ export const NavBar = function ({user, budgets, Methods}) {
         });
     };
 
-    const confirmRechargeAll = function () {
-        setModalOpened(true);
+    const confirmRechargeAll = function (e) {
+        e.preventDefault();
+        setRechargeAllModalOpened(true);
+    };
+
+    const newBudgetAction = function (e) {
+        e.preventDefault();
+        setNewBudgetModalOpened(true);
     };
 
     const totalWeeklyBudget = budgets.reduce(function (acc, item) {
@@ -44,7 +73,12 @@ export const NavBar = function ({user, budgets, Methods}) {
 
     return (
         <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-            <Confirm opened={modalOpened} setOpened={setModalOpened} action={rechargeAll} />
+            <RechargeAllModal
+                opened={rechargeAllModalOpened}
+                setOpened={setRechargeAllModalOpened}
+                action={rechargeAll}
+            />
+            <NewBudgetModal opened={newBudgetModalOpened} setOpened={setNewBudgetModalOpened} action={function () {}} />
             <div className="container-fluid">
                 <span className="navbar-brand">
                     My Budgets <sub>{toCurrency(totalWeeklyBudget)}</sub>
@@ -63,19 +97,12 @@ export const NavBar = function ({user, budgets, Methods}) {
                 <div className="collapse navbar-collapse" id="navbarCollapse">
                     <ul className="navbar-nav me-auto mb-2 mb-md-0">
                         <li className="nav-item">
-                            <a
-                                className="nav-link"
-                                href="#"
-                                onClick={function (e) {
-                                    e.preventDefault();
-                                    confirmRechargeAll();
-                                }}
-                            >
+                            <a className="nav-link" href="#" onClick={confirmRechargeAll}>
                                 Recharge all
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link disabled" href="#" aria-disabled="true">
+                            <a className="nav-link" href="#" onClick={newBudgetAction}>
                                 New budget
                             </a>
                         </li>
